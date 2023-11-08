4a2f9678dd694b3d18626669d129bf1ce49f4f18
Emanuele <rosi.1812180@studenti.uniroma1.it>
2023-11-08 14:47:29 +0100

    Optimized two and three points analyzers

--- analysis-kkbar/reader_2pts.py
+++ analysis-kkbar/reader_2pts.py
@@ -1,24 +1,80 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 home_dir = '/Users/emanuelerosi/Thesis_MSc/kaons-oscillations/tm-mesons-obc/mesons-master/dat/'
 file_name = 'mesons_run_name.mesons.dat'
 path_to_file = home_dir+file_name
 
-######################## FUNCTIONS, TOOLS ########################
+# this must be set by the user 
+N1=8
+N2=8
+N3=8
+volume3D=N1*N2*N3   # number of lattice points for each timeslice
 
-def printRed(skk):
-    print("\033[91m{}\033[00m".format(skk))
-def prCyan(skk):
-    print("\033[96m{}\033[00m".format(skk))
+######################## TOOLS ########################
+
+def printRed(message):
+    print("\033[91m{}\033[00m".format(message))
+def printCyan(message):
+    print("\033[96m{}\033[00m".format(message))
 
 def error_check(condition,error_message,optional_message=''):
     if(condition):
         printRed('ERROR:'+optional_message)
         printRed(error_message)
         quit()
+        
+def noise_to_string(type):
+    match type:
+        case 0:
+            return 'Z2'
+        case 1:
+            return 'GAUSS'
+        case 2:
+            return 'U1'
+        case _:
+            error_check(True,"Invalid noise type")
+        
+def dirac_to_str(type):
+    match type:
+        case 0:
+            return 'G0'
+        case 1:
+            return 'G1'
+        case 2:
+            return 'G2'
+        case 3:
+            return 'G3'
+        case 5:
+            return 'G5'
+        case 6:
+            return '1'
+        case 7:
+            return 'G0G1'
+        case 8:
+            return 'G0G2'
+        case 9:
+            return 'G0G3'
+        case 10:
+            return 'G0G5'
+        case 11:
+            return 'G1G2'
+        case 12:
+            return 'G1G3'
+        case 13:
+            return 'G1G5'
+        case 14:
+            return 'G2G3'
+        case 15:
+            return 'G2G5'
+        case 16:
+            return 'G3G5'
+        case _:
+            error_check(True,'Invalid Dirac matrix: '+str(type))
 
 ########################## DATA READING ##########################
+    # LEGEND:
     # dtypes:
     #   np.int16    --> i2
     #   np.int32    --> i4
@@ -33,11 +89,11 @@ ncorr=int_reads[0]
 nnoise=int_reads[1]
 ntimes=int_reads[2]
 noisetype=int_reads[3]
-print(  '\nGeneral settings:'
-        '\n\tNumber of correlators Ncorr =',ncorr,
-        '\n\tNumber of noise spinors Nnoise =',nnoise,
-        '\n\tNumber of timeslices N_T =',ntimes,
-        '\n\tNoisetype:',noisetype)
+printCyan('\nGeneral settings:')
+print(  '\tNumber of correlators =',ncorr,
+        '\n\tNumber of noise spinors =',nnoise,
+        '\n\tNumber of timeslices =',ntimes,
+        '\n\tNoisetype:',noise_to_string(noisetype),'\n')
 
 kappas=[[0]*2]*ncorr
 mus=[[0]*2]*ncorr
@@ -47,7 +103,7 @@ x0=[0]*ncorr
 isreal=[0]*ncorr
 
 for icorr in np.arange(0,ncorr,1):
-    print("\nCorrelator #",icorr)
+    printCyan("Correlator #"+str(icorr))
     offset_byte = 4*4 + icorr*(4*8+4*4)
     tmp=np.fromfile(path_to_file,dtype='<f8',offset=offset_byte,count=4)
     kappas[icorr]=[tmp[0],tmp[1]]
@@ -57,46 +113,76 @@ for icorr in np.arange(0,ncorr,1):
     type2[icorr]=tmp[1]
     x0[icorr]=tmp[2]
     isreal[icorr]=tmp[3]
-    tmp_string=[0,0,0,0]
+    tmp_string=[0,0]
     for idx,val in enumerate(kappas[icorr]):
         tmp_string[idx] = '{:.3f}'.format(val)
-    print(  '\tHopping parameters for each correlator:',tmp_string,
+    print(  '\tHopping parameters:',tmp_string,
             '\n\tTwisted masses:',mus[icorr],
-            '\n\tOperator in source y0',type2[icorr],
-            '\n\tOperator in source x0',type1[icorr],
-            '\n\tIsreal:',isreal[icorr])
+            '\n\tOperator in source y0:',dirac_to_str(type2[icorr]),
+            '\n\tOperator in source x0:',dirac_to_str(type1[icorr]),
+            '\n\tIsreal:',bool(isreal[icorr]),'\n')
 
-# expected data length
-numcount=nnoise*ntimes*sum(np.array([2]*ncorr)-isreal)
-# number of bytes already read
+# read configuration number
 offset_byte=4*4 + ncorr*(4*8+4*4)
-# read data
-raw_data = np.fromfile(path_to_file,dtype='<f8',offset=offset_byte)
-error_check(numcount!=len(raw_data),'len(data)!=expected lenght')
+configuration_number=np.fromfile(path_to_file,dtype='<i4',offset=offset_byte,count=1)
+printCyan('Configuration number:'+str(configuration_number[0])+' --> loaded\n')
+offset_byte+=4
+
+# read raw data
+data_count=nnoise*ntimes*sum(np.array([2]*ncorr)-isreal)
+raw_data=np.fromfile(path_to_file,dtype='<f8',offset=offset_byte)
+error_check(data_count!=len(raw_data),'len(data)!=expected lenght')
 
 ########################## DATA REORDERING ##########################
 
-data=[[[0]*ntimes]*ncorr]*2
-for i1 in np.arange(0,nnoise,1):
-    for time in np.arange(0,ntimes,1):
-        for corr in np.arange(0,ncorr,1):
-            idx=i1 + nnoise*(time+ntimes*corr)
-            data[0][corr][time] += raw_data[idx]        # real part
-            if(isreal[corr]==0):                        # immaginary part
-                data[1][corr][time] += raw_data[idx] 
-                
-data = np.array(data)
-                
-########################## CORRELATOR PLOT ##########################             
+# create an ordered data structure
+offset_idx=0
+data = np.array([[[0]*ntimes]*ncorr]*2)
+    
+for icorr in np.arange(0,ncorr,1):
+    if(isreal[icorr]==True):
+        for time in np.arange(0,ntimes,1):
+            for inoise in np.arange(0,nnoise,1):
+                idx=offset_idx+inoise+nnoise*time
+                data[0][icorr][time] += raw_data[idx]   # only real part
+    else:
+        for time in np.arange(0,ntimes,1):
+            for inoise in np.arange(0,nnoise,1):
+                idx=offset_idx+2*(inoise+nnoise*time)
+                data[0][icorr][time] += raw_data[idx]   # real part
+                data[1][icorr][time] += raw_data[idx+1] # immaginary part
+    offset_idx+=nnoise*ntimes*(2-isreal[icorr])
 
-fig = plt.figure(figsize=(13,5),dpi=300)
+# divide by the norm - simulation doesn't do it
+for idx,value in enumerate(data):
+    data[idx]=value/volume3D
+    
+########################## CORRELATORS PLOT ##########################             
+
+# Real part of correlators
+real_plot=plt.figure(1,figsize=(13,5),dpi=300)
+plt.title(r'Correlators $C_{M\overline{M}}(x_4,y_4) = \frac{a^3}{N_{sp}}\sum_{\vec{x},\vec{y}} < M(x) \overline{M}(y) >$')
 plt.xlabel("Timeslice $y_4$")
-plt.ylabel("Real value of the correlator")
+plt.ylabel("Real value of the correlators")
 plt.xlim(0,ntimes)
-if(min(tmp)!=max(tmp)):
-    plt.ylim(min(data[0][0])*0.9,max(data[0][0])*1.12)
 plt.grid()
-plt.plot(data[0][0],'1',c='darkblue', alpha=0.7,lw=0.7)
-corr_name = '$\mathbb{Re}(C_{PS+SP}(x_4,y_4,z_4))$'
-plt.title('Correlator: '+corr_name)
-fig.savefig('plots/sampleplot_2pts.png',dpi=fig.dpi)
+for icorr in np.arange(0,ncorr,1):
+    plt.plot(data[0][icorr],str(icorr+1),label='correlator #'+str(icorr),alpha=1,lw=0.75)
+plt.legend(loc='upper right')
+
+# Immaginary part of correlators
+immaginary_plot=plt.figure(2,figsize=(13,5),dpi=300)
+plt.title(r'Correlators $C_{M\overline{M}}(x_4,y_4) = \frac{a^3}{N_{sp}}\sum_{\vec{x},\vec{y}} < M(x) \overline{M}(y) >$')
+plt.xlabel("Timeslice $y_4$")
+plt.ylabel("Immaginary value of the correlators")
+plt.xlim(0,ntimes)
+plt.grid()
+for icorr in np.arange(0,ncorr,1):
+    plt.plot(data[1][icorr],str(icorr+1),label='correlator #'+str(icorr),alpha=1,lw=0.75)
+plt.legend(loc='upper right')
+
+# Save plots in a single file
+pp = PdfPages("plots/Plots_2pts.pdf")
+pp.savefig(real_plot, dpi=real_plot.dpi, transparent = True)
+pp.savefig(immaginary_plot, dpi=immaginary_plot.dpi, transparent = True)
+pp.close()
--- analysis-kkbar/reader_3pts.py
+++ analysis-kkbar/reader_3pts.py
@@ -1,15 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 home_dir = '/Users/emanuelerosi/Thesis_MSc/kaons-oscillations/tm-mesons-obc/mesons-master/dat/'
 file_name = 'crrltrs_run_name.correlators.dat'
 path_to_file = home_dir+file_name
 
+# this must be set by the user 
+N1=8
+N2=8
+N3=8
+volume3D=N1*N2*N3   # number of lattice points for each timeslice
+
 ######################## FUNCTIONS, TOOLS ########################
 
 def printRed(skk):
     print("\033[91m{}\033[00m".format(skk))
-def prCyan(skk):
+def printCyan(skk):
     print("\033[96m{}\033[00m".format(skk))
 
 def error_check(condition,error_message,optional_message=''):
@@ -17,8 +24,82 @@ def error_check(condition,error_message,optional_message=''):
         printRed('ERROR:'+optional_message)
         printRed(error_message)
         quit()
+        
+def noise_to_string(type):
+    match type:
+        case 0:
+            return 'Z2'
+        case 1:
+            return 'GAUSS'
+        case 2:
+            return 'U1'
+        case _:
+            error_check(True,"Invalid noise type")
+                    
+def dirac_to_str(type):
+    match type:
+        case 0:
+            return 'G0'
+        case 1:
+            return 'G1'
+        case 2:
+            return 'G2'
+        case 3:
+            return 'G3'
+        case 5:
+            return 'G5'
+        case 6:
+            return '1'
+        case 7:
+            return 'G0G1'
+        case 8:
+            return 'G0G2'
+        case 9:
+            return 'G0G3'
+        case 10:
+            return 'G0G5'
+        case 11:
+            return 'G1G2'
+        case 12:
+            return 'G1G3'
+        case 13:
+            return 'G1G5'
+        case 14:
+            return 'G2G3'
+        case 15:
+            return 'G2G5'
+        case 16:
+            return 'G3G5'
+        case _:
+            error_check(1,'Invalid Dirac matrix: '+str(type))
+            
+def operator_to_str(type):
+    match type:
+        case 0:
+            return 'SS'
+        case 1:
+            return 'PP'
+        case 2:
+            return 'SP'
+        case 3:
+            return 'PS'
+        case 4:
+            return 'VV'
+        case 5:
+            return 'AA'
+        case 6:
+            return 'VA'
+        case 7:
+            return 'AV'
+        case 8:
+            return 'TT'
+        case 9:
+            return 'TTt'
+        case _:
+            error_check(1,'Invalid operator matrix: '+str(type))
 
 ########################## DATA READING ##########################
+    # LEGEND:
     # dtypes:
     #   np.int16    --> i2
     #   np.int32    --> i4
@@ -27,6 +108,7 @@ def error_check(condition,error_message,optional_message=''):
     # use <f for LITTLE_ENDIAN
     # use >f for BIG_ENDIAN
 
+# read file_head
 int_reads = np.fromfile(path_to_file,dtype='<i4',offset=0,count=6)
 ncorr=int_reads[0]
 nnoise=int_reads[1]
@@ -34,12 +116,13 @@ ntimes=int_reads[2]
 noisetype=int_reads[3]
 x0=int_reads[4]
 z0=int_reads[5]
-print(  '\nGeneral settings:'
-        '\n\tNumber of correlators Ncorr =',ncorr,
+printCyan('\nGeneral settings:')
+print(  '\tNumber of correlators Ncorr =',ncorr,
         '\n\tNumber of noise spinors Nnoise =',nnoise,
         '\n\tNumber of timeslices N_T =',ntimes,
-        '\n\tNoisetype:',noisetype,
-        '\n\tSources timeslices: z0 =',z0,' x0 =',x0)
+        '\n\tNoisetype:',noise_to_string(noisetype),
+        '\n\tSource timeslice: x0 =',x0,
+        '\n\tSource timeslice: z0 =',z0,'\n')
 
 kappas=[[0]*4]*ncorr
 mus=[[0]*4]*ncorr
@@ -49,7 +132,7 @@ operator_type=[0]*ncorr
 isreal=[0]*ncorr
 
 for icorr in np.arange(0,ncorr,1):
-    print("\nCorrelator #",icorr)
+    printCyan("Correlator #"+str(icorr))
     offset_byte = 6*4 + icorr*(8*8+4*4)
     tmp=np.fromfile(path_to_file,dtype='<f8',offset=offset_byte,count=8)
     kappas[icorr]=[tmp[0],tmp[1],tmp[2],tmp[3]]
@@ -62,48 +145,105 @@ for icorr in np.arange(0,ncorr,1):
     tmp_string=[0,0,0,0]
     for idx,val in enumerate(kappas[icorr]):
         tmp_string[idx] = '{:.3f}'.format(val)
-    print(  '\tHopping parameters for each correlator:',tmp_string,
+    print(  '\tHopping parameters:',tmp_string,
             '\n\tTwisted masses:',mus[icorr],
-            '\n\tOperator in source z0',typeC_z[icorr],
-            '\n\tOperator in source x0',typeA_x[icorr],
-            '\n\tIsreal:',isreal[icorr])
-    
-numcount=2*ncorr*nnoise*nnoise*ntimes
-offset_byte=6*4 + ncorr*(8*8+4*4)
-raw_data = np.fromfile(path_to_file,dtype='<f8',offset=offset_byte)
-error_check(numcount!=len(raw_data),'len(data)!=expected lenght')
+            '\n\tGammaA:',dirac_to_str(typeC_z[icorr]),
+            '\n\tGammaC:',dirac_to_str(typeA_x[icorr]),
+            '\n\tMixing operator:',operator_to_str(operator_type[icorr]),
+            '\n\tIsreal:',bool(isreal[icorr]),'\n')
+ 
+# read configuration number
+offset_byte=6*4+ncorr*(8*8+4*4)
+configuration_number=np.fromfile(path_to_file,dtype='<i4',offset=offset_byte,count=1)
+printCyan('Configuration number:'+str(configuration_number[0])+' --> loaded')
+offset_byte+=4
+   
+# read data
+data_count=2*ncorr*nnoise*nnoise*ntimes
+raw_data=np.fromfile(path_to_file,dtype='<f8',offset=offset_byte)
+error_check(data_count!=len(raw_data),'len(data)!=expected lenght')
 
 ########################## DATA REORDERING ##########################
 
-data=[[[0]*2]*ntimes]*ncorr
-for i2 in np.arange(0,nnoise,1):
-    for i1 in np.arange(0,nnoise,1):
+# create an ordered data structure
+offset_idx=0
+data= np.array([[[0]*2]*ntimes]*ncorr)
+
+#for corr in np.arange(0,ncorr,1):
+#    for time in np.arange(0,ntimes,1):
+#        for i1 in np.arange(0,nnoise,1):
+#            for i2 in np.arange(0,nnoise,1):
+#                if(isreal[corr]==True):
+#                    idx=offset_idx + i2 + i1*nnoise + time*nnoise*nnoise
+#                    data[corr][time][0] += raw_data[idx]
+#                else:
+#                    idx=offset_idx + 2*(i2 + i1*nnoise + time*nnoise*nnoise)
+#                    data[corr][time][0] += raw_data[idx]
+#                    data[corr][time][1] += raw_data[idx+1]
+#    offset_idx+=nnoise*nnoise*ntimes*(2-isreal[corr])
+    
+for corr in np.arange(0,ncorr,1):
+    if(isreal[corr]==True):
         for time in np.arange(0,ntimes,1):
-            for corr in np.arange(0,ncorr,1):
-                idx=i2 + nnoise*i1 + nnoise*nnoise*(time+ntimes*corr)
-                data[corr][time][0] += raw_data[2*idx]      # real part
-                data[corr][time][1] += raw_data[2*idx+1]    # immaginary part
-                
-data = np.array(data)
+            for i1 in np.arange(0,nnoise,1):
+                for i2 in np.arange(0,nnoise,1):
+                    idx=offset_idx + i2 + i1*nnoise + time*nnoise*nnoise
+                    data[corr][time][0] += raw_data[idx]    # only real part
+    else:
+        for time in np.arange(0,ntimes,1):
+            for i1 in np.arange(0,nnoise,1):
+                for i2 in np.arange(0,nnoise,1):
+                    idx=offset_idx + 2*(i2 + i1*nnoise + time*nnoise*nnoise)
+                    data[corr][time][0] += raw_data[idx]    # real part
+                    data[corr][time][1] += raw_data[idx+1]  # immaginary part
+    offset_idx+=nnoise*nnoise*ntimes*(2-isreal[corr])
+
+# I divide by the norm - simulation doesn't do it
+for idx,value in enumerate(data):
+    data[idx]=value/volume3D
                 
 ########################## CORRELATOR PLOT ##########################             
 
 # The first correlator is PS, the second SP. Then:
-corr_PSpSP = data[0] + data[1]
-corr_PSmSP = data[0] - data[1]
+correlators = [data[0]-data[1],data[0]+data[1]]
 
-tmp = np.array([0]*len(corr_PSpSP))
-for idx,val in enumerate(corr_PSpSP):
-    tmp[idx]=val[0]    
-
-fig = plt.figure(figsize=(13,5),dpi=300)
+for i,corr in enumerate(correlators):
+    tmp_re = np.array([0]*len(corr))
+    tmp_im = np.array([0]*len(corr))
+    for idx,val in enumerate(corr):
+        tmp_re[idx]=val[0]
+        tmp_im[idx]=val[1]
+        
+# Real part of correlators
+real_plot=plt.figure(1,figsize=(13,5),dpi=300)
+plt.title(r'Correlators: $\mathbb{Re}\left\{C_{i[+]}(x_4,y_4,z_4)\right\}$')
 plt.xlabel("Timeslice $y_4$")
 plt.ylabel("Real value of the correlator")
 plt.xlim(0,ntimes)
-if(min(tmp)!=max(tmp)):
-    plt.ylim(min(tmp)*0.9,max(tmp)*1.12)
 plt.grid()
-plt.plot(tmp,'1',c='darkred', alpha=0.7,lw=0.9)
-corr_name = '$\mathbb{Re}(C_{PS+SP}(x_4,y_4,z_4))$'
-plt.title('Correlator: '+corr_name)
-fig.savefig('plots/sampleplot_3pts.png',dpi=fig.dpi)
+for idx,corr in enumerate(correlators):
+    tmp = np.array([0]*len(corr))
+    for i,val in enumerate(corr):
+        tmp[i]=val[0]
+    plt.plot(tmp,str(idx+1),label='correlator #'+str(idx),alpha=1,lw=0.75)
+plt.legend(loc='upper right')
+
+# Immaginary part of correlators
+immaginary_plot=plt.figure(2,figsize=(13,5),dpi=300)
+plt.title(r'Correlators: $\mathbb{Im}\left\{C_{i[+]}(x_4,y_4,z_4)\right\}$')
+plt.xlabel("Timeslice $y_4$")
+plt.ylabel("Immaginary value of the correlator")
+plt.xlim(0,ntimes)
+plt.grid()
+for idx,corr in enumerate(correlators):
+    tmp = np.array([0]*len(corr))
+    for i,val in enumerate(corr):
+        tmp[i]=val[1]
+    plt.plot(tmp,str(idx+1),label='correlator #'+str(idx),alpha=1,lw=0.75)
+plt.legend(loc='upper right')    
+
+# Save plots in a single file
+pp = PdfPages("plots/Plots_3pts.pdf")
+pp.savefig(real_plot, dpi=real_plot.dpi, transparent = True)
+pp.savefig(immaginary_plot, dpi=immaginary_plot.dpi, transparent = True)
+pp.close()
